f019e68a55f05fc2e7eb85d823fdb46dae42dc89
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-12-03 19:40:31 +0300

    init commit

---
+++ 47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(int rows, int columns, float MinValue, float MaxValue)
+{
+    double[,] matrix = new double[rows, columns];
+  
+   Random rnd = new Random();
+   
+    for (int i = 0; i < matrix.GetLength(0); i++) //3
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(2)
+        { 
+             matrix[i, j] =  Math.Round(rnd.NextDouble()*((MaxValue-MinValue)+MinValue),2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2D = CreateMatrixRndDouble(3, 4,-1,10);
+PrintMatrix(array2D);
